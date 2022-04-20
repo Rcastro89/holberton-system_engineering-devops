18362cd5b27716443ea0b225693c3383a61b4453
Rcastro89 <r_castro_m@yahoo.es>
2022-04-19 22:15:48 -0500

    lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # first puppet create file
 
-file {'create_files':
-	ensure => 'file',
-	path => '/tmp/school',
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => 'I love Puppet'
+file { 'create_files':
+  ensure  => 'file',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
