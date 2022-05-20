39c134f7ea7e48d5031e3cff288ac5ef1cf6c11b
Rcastro89 <r_castro_m@yahoo.es>
2022-05-20 14:50:16 -0500

    todo

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""accessing a url with employee ID to return information permision"""
+
+
+if __name__ == "__main__":
+    import json
+    import requests
+
+    user = requests.get("https://jsonplaceholder.typicode.com/users").json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+    with open('todo_all_employees.json', 'w') as archivo:
+            json.dump({x['id']: list({
+            "username": x['username'],
+            "task": i["title"],
+            "completed": i["completed"]
+            } for i in todo if i['userId'] == x['id']) for x in user}, archivo)
