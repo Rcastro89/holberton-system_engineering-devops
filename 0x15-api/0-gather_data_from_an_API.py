46a374709636bf4f65f1c0b50c8e27610717a69a
Rcastro89 <r_castro_m@yahoo.es>
2022-05-19 22:18:59 -0500

    update 5

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
-"""Escriba un script de Python que, utilizando esta API REST,
-para una ID de empleado determinada, devuelva información
-sobre el progreso de su lista TODO."""
-
-import requests
-import sys
+"""accessing a url with employee ID to return information."""
 
 if __name__ == "__main__":
+    import requests
+    from sys import argv
 
-    did = sys.argv[1]
-    url = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                       .format(did))
-    nombre = url.json().get('name')
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos").json()
-    total = 0
-    completed = 0
-    for tareas in todos:
-        if tareas.get('userId') == int(did):
-            total += 1
-            if tareas.get('completed'):
-                completed += 1
-    print("Employee {} is done with tasks({}/{}):"
-          .format(nombre, completed, total))
-    print("\n".join(["\t " + tareas.get('title') for tareas in todos
-                     if tareas.get('userId') == int(did)
-                     and tareas.get('completed')]))
+    ID = int(argv[1])
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(ID)).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(ID)).json()
+    tasks = []
+    for task in todo:
+        if task.get('completed') is True:
+            tasks.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(tasks), len(todo)))
+    for task in tasks:
+        print("\t {}".format(task))
