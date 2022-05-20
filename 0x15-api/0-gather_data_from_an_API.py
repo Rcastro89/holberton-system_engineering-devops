be582955b2a2ef3c983a5fe17ccf20093378a52e
Rcastro89 <r_castro_m@yahoo.es>
2022-05-19 21:36:07 -0500

    update 2

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,34 +3,17 @@
 
 import json
 import sys
-import urllib.request
+import requests
 
 if __name__ == "__main__":
-    req = urllib.request.Request('https://jsonplaceholder.typicode.com/todos')
-    with urllib.request.urlopen(req) as response:
-        tareas = response.read()
-    tareas = tareas.decode('utf8')
-    tareas = json.loads(tareas)
-    req = urllib.request.Request('https://jsonplaceholder.typicode.com/users')
-    with urllib.request.urlopen(req) as response:
-        usuarios = response.read()
-    usuarios = usuarios.decode('utf8')
-    usuarios = json.loads(usuarios)
-    nombre = ""
-    completadas = 0
-    total = 0
-    titulos = []
-    for i in range(0, len(usuarios)):
-        if usuarios[i]['id'] == int(sys.argv[1]):
-            nombre = usuarios[i]['name']
-            break
-    for x in range(0, len(tareas)):
-        if tareas[x]['userId'] == int(sys.argv[1]):
-            total += 1
-            if tareas[x]['completed'] is True:
-                completadas += 1
-                titulos.append(tareas[x]['title'])
-    print("Employee {} is done with tasks({}/{}):".format
-          (nombre, completadas, total))
-    for v in titulos:
-        print("\t {}".format(v))
+    ruta = 'https://jsonplaceholder.typicode.com/users/'
+    usuarios = requests.get(ruta + sys.argv[1])
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos')
+    user_name = json.loads(usuarios.text)['name']
+    todo = json.loads(todo.text)
+    tasks = list(i for i in todo if i['userId'] == int(sys.argv[1]))
+    succed_tasks = list(i for i in tasks if i['completed'] is True)
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(user_name, len(succed_tasks), len(tasks)))
+    [print('\t ' + task['title']) for task in succed_tasks]
