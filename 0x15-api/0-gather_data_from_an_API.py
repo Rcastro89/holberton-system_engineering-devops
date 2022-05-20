f6fe65f3e2f00a30fb7f6bd2b7bb2543fd989665
Rcastro89 <r_castro_m@yahoo.es>
2022-05-19 21:10:40 -0500

    0 first api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import urllib.request
+import json
+import sys
+
+req = urllib.request.Request('https://jsonplaceholder.typicode.com/todos/')
+with urllib.request.urlopen(req) as response:
+   tareas = response.read()
+tareas = tareas.decode('utf8')
+tareas = json.loads(tareas)
+req = urllib.request.Request('https://jsonplaceholder.typicode.com/users/')
+with urllib.request.urlopen(req) as response:
+   usuarios = response.read()
+usuarios = usuarios.decode('utf8')
+usuarios = json.loads(usuarios)
+nombre = ""
+completadas = 0
+total = 0
+titulos = []
+for i in range(0, len(usuarios)):
+   if usuarios[i]['id'] == int(sys.argv[1]):
+      nombre = usuarios[i]['name']
+      break
+for x in range(0, len(tareas)):
+   if tareas[x]['userId'] == int(sys.argv[1]):
+      total += 1
+      if tareas[x]['completed'] == True:
+         completadas += 1
+         titulos.append(tareas[x]['title'])
+print("Employee {} is done with tasks({}/{}):".format(nombre,completadas,total))
+for v in titulos:
+   print("\t {}".format(v)) 
