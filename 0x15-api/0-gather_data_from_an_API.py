7385d1fd394e075df26b2a6cc5345465c9ad8b26
Rcastro89 <r_castro_m@yahoo.es>
2022-05-19 21:20:13 -0500

    0 first api 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,12 +5,12 @@ import sys
 
 req = urllib.request.Request('https://jsonplaceholder.typicode.com/todos/')
 with urllib.request.urlopen(req) as response:
-   tareas = response.read()
+    tareas = response.read()
 tareas = tareas.decode('utf8')
 tareas = json.loads(tareas)
 req = urllib.request.Request('https://jsonplaceholder.typicode.com/users/')
 with urllib.request.urlopen(req) as response:
-   usuarios = response.read()
+    usuarios = response.read()
 usuarios = usuarios.decode('utf8')
 usuarios = json.loads(usuarios)
 nombre = ""
@@ -18,15 +18,16 @@ completadas = 0
 total = 0
 titulos = []
 for i in range(0, len(usuarios)):
-   if usuarios[i]['id'] == int(sys.argv[1]):
-      nombre = usuarios[i]['name']
-      break
+    if usuarios[i]['id'] == int(sys.argv[1]):
+        nombre = usuarios[i]['name']
+        break
 for x in range(0, len(tareas)):
-   if tareas[x]['userId'] == int(sys.argv[1]):
-      total += 1
-      if tareas[x]['completed'] == True:
-         completadas += 1
-         titulos.append(tareas[x]['title'])
-print("Employee {} is done with tasks({}/{}):".format(nombre,completadas,total))
+    if tareas[x]['userId'] == int(sys.argv[1]):
+        total += 1
+        if tareas[x]['completed'] is True:
+            completadas += 1
+            titulos.append(tareas[x]['title'])
+print("Employee {} is done with tasks({}/{}):".format
+      (nombre, completadas, total))
 for v in titulos:
-   print("\t {}".format(v)) 
+    print("\t {}".format(v))
