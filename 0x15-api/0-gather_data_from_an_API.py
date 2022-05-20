1891cf45f51faed967bf9e5074ec578620348196
Rcastro89 <r_castro_m@yahoo.es>
2022-05-19 21:38:44 -0500

    update 3

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,14 +6,28 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    ruta = 'https://jsonplaceholder.typicode.com/users/'
-    usuarios = requests.get(ruta + sys.argv[1])
-    todo = requests.get('https://jsonplaceholder.typicode.com/todos')
-    user_name = json.loads(usuarios.text)['name']
-    todo = json.loads(todo.text)
-    tasks = list(i for i in todo if i['userId'] == int(sys.argv[1]))
-    succed_tasks = list(i for i in tasks if i['completed'] is True)
 
-    print('Employee {} is done with tasks({}/{}):'
-          .format(user_name, len(succed_tasks), len(tasks)))
-    [print('\t ' + task['title']) for task in succed_tasks]
+    argv = sys.argv[1]
+    name = (
+        requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                     .format(argv))
+        .json()
+        .get("name")
+    )
+
+    taskEmploy = requests.get(
+        "https://jsonplaceholder.typicode.com/todos"
+    ).json()
+    tasksDone = 0
+    tasksTotal = 0
+    nameTaskDone = []
+    for task in taskEmploy:
+        if task.get("completed") and task.get('userId') == int(argv):
+            tasksDone += 1
+            nameTaskDone.append(task.get("title"))
+        if task.get('userId') == int(argv):
+            tasksTotal += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, tasksDone, tasksTotal))
+    print("\t ", end="")
+    print("\n\t ".join(nameTaskDone))
